a20b0daa6b9b5fa071853731cd118e1cdfe86912
Lucas D'Avila <lqdavila11@gmail.com>
2020-10-22 10:11:22 -0700

    Inital Solution Setup

---
+++ src/CrackingCodingInterview/CrackingCodingInterview.Core/Models/Common/API/BaseResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace CrackingCodingInterview.Core.Common
+{
+    public class BaseResponse
+    {
+        public string Language { get; set; } = "en";
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public object Result { get; set; } = null;
+    }
+}
---
+++ src/CrackingCodingInterview/CrackingCodingInterview.Web/Controllers/v1/BaseController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CrackingCodingInterview.Core.Common;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CrackingCodingInterview.Web.Controllers.v1
+{
+    [Route("api/v1")]
+    public class BaseController : Controller
+    {
+        // GET: api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var response = new BaseResponse()
+            {
+                Success = true,
+                Message = "Welcome to Cracking the Coding Interview Functions!"
+            };
+
+            return Ok(response);
+        }
+
+        
+    }
+}
